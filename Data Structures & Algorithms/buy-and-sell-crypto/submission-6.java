2f7681f553253ceb8d591795fcb6a5adf8abe427
jonathan <117922727+jon-rod21@users.noreply.github.com>
2026-08-23 18:18:58 -0500

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int left = 0;
+        int right = 1;
+
+        int max = 0;
+
+        while (right < prices.length)
+        {
+            if (prices[left] < prices[right])
+            {
+                int price = prices[right] - prices[left];
+                max = Math.max(max, price);
+            }
+            else
+            {
+                left = right;
+            }
+            right++;
+        }
+        return max;
+    }
+}
